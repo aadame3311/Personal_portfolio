6bed34f58b0afc0b97dfa4ae26c4a1a8b235487d
Antonio Adame <antonio.adame01@utrgv.edu>
2020-01-03 16:33:05 -0600

    re-designed the work experience page. Also removed some sections

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ app.set('view engine', 'handlebars');
 const path = require('path'); // Path module.
 
 // Serve static files. 
-app.use(express.static(path.join(__dirname, './public/')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, './public/vendor')));
+app.use(express.static(path.join(__dirname, './public/img/logos/')));
 // Get twitter 
 var client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -25,56 +26,91 @@ var client = new Twitter({
 
 
 app.get('/', (req, res) => {
+    // Defined basic personal details so I could interploate them into the bio section
+    const personal_details = {
+        first_name: "Antonio",
+        last_name: "Adame Puente",
+        title: "Software Engineer",
+        address: "2117 Rice Ave",
+        city: "McAllen", 
+        state: "TX",
+        zip: "78504",
+        email: "a.adame59@yahoo.com",
+    }
+    // This object is used in the HTML Handlebars template
     const personal_information = {
         "About": {
-            first_name: "Antonio",
-            last_name: "Adame",
-            address: "1107 Shavano Dr.",
-            city: "Edinburg",
-            state: "Texas",
-            zip: "78541",
-            phone: "(956) 438-8893",
-            email: "a.adame59@yahoo.com",
-            bio: ["I am currently a Computer Science undergraduate student at the University of Texas Rio Grande Valley.", 
-                    "I have a keen eye for detail and I am a strong believer of building software to make an impact in the world."],
+            ...personal_details,
+            bio: `${personal_details.title} currently based in the ${personal_details.city}, ${personal_details.state} area. `+
+                `I personally enjoy working on web and mobile platforms.\n`+
+                `Although I'm comfortable learning and using new technologies, I currently focus on Flutter, React.js, Node.js and .NET.`,
             linkedin_profile: "https://www.linkedin.com/in/antonio-adame-757900172/",
             github_profile: "https://www.linkedin.com/in/antonio-adame-757900172/",
             twitter_profile: "https://twitter.com/AntonioAdame3",
-            instagram_profile: "https://www.instagram.com/a.adame.p/",
+            instagram_profile: "https://www.instagram.com/antonioadame.p/",
         },
         "Experience": [
             {
-                title: "Teaching Assistant",
-                company: "The College of Engineering & Computer Science @ UTRGV",
-                address: "1201 W University Dr. Edinburg, Texas 78541",
-                timeline: "January 2019 - Present",
-                description: "Assisted university professors by grading and tutoring students for graduate course Foundations of Algorithms & Programming Languages as well as undergraduate course Computer Science 1. \n"+
-                                "Improved student performance by around 12.5% by personally setting up computer science 1 study sessions and meeting times with FAPL graduate students."
+                title: "Software Engineer I",
+                company: "Invoice Cloud, Inc.",
+                address: "",
+                timeline: "01/2020 - Present",
+                description: [],
+                company_logo: "ic.jpg",
             },
             {
-                title: "Student Assistant",
-                company: "Data Center @ UTRGV",
+                title: "Web App Developer Intern",
+                company: "UTRGV School of Medicine",
                 address: "1201 W University Dr. Edinburg, Texas 78541",
-                timeline: "October 2017 - January 2018",
-                description: "Assisted with Windows and Linux virtual system deployment and Data Center maintenance consisting of 900+ virtual/physical servers."
+                timeline: "06/2019 - 12/2019",
+                description: [
+                    "Designed and implemented cloud infrastructure for a data warehouse",
+                    "Developed base components for faculty dashboard which reports faculty metrics and performance",
+                ],
+                company_logo: "utrgv_som.png",
+            },
+            {
+                title: "Teaching Assistant",
+                company: "UTRGV CECS",
+                address: "1201 W University Dr. Edinburg, Texas 78541",
+                timeline: "01/2019 - 05/2019",
+                description: "Assisted university professors by grading and tutoring students for graduate course Foundations of Algorithms & Programming Languages as well as undergraduate course Computer Science 1. \n"+
+                                "Improved student performance by around 12.5% by personally setting up computer science 1 study sessions and meeting times with FAPL graduate students.",
+                company_logo: "utrgv.png",
             },
         ],
         "Education": [
             {
                 school: "The University of Texas Rio Grande Valley",
-                degree: "Bachelor of Science",
-                timeline: "August 2014 - Present",
+                degree: "Bachelor's of Science",
+                timeline: "December 2019",
                 major: "Computer Science",
-                gpa: "GPA: 3.27",
+                gpa: "GPA: 3.35",
             },
 
             
         ],
         "Skills": {
-            programming_languages: ["java", "python", "html5", "css3-alt", "js-square", "react", "node-js", "sass", "npm"],
+            programming_languages: [
+                "java", 
+                "python", 
+                "html5", 
+                "css3-alt", 
+                "js-square", 
+                "react", 
+                "node-js", 
+                "sass", 
+                "npm"
+            ],
         },
-        "Interests": "Apart from being passionate about software engineering, I enjoy playing music. Like most people, music is a very big part of my life. I primarily play acoustic guitar though I strum on an electric one every so often. If I’m not making music, I’m probably playing video games. Recently, I have been interested in more competitive games such as Rainbow Six and Super Smash Bros \n"+
-                            "Aside from video games and music, I also enjoy TV Shows and manga. My favorite TV Series is Game of Thrones and my favorite manga of all time is One Piece.\n",
+        "Interests": "Apart from being passionate about software engineering, " +
+                    "I enjoy playing music. Like most people, music is a very big part " +
+                    "of my life. I primarily play acoustic guitar though I strum on an " +
+                    "electric one every so often. If I’m not making music, I’m probably " +
+                    "playing video games. Recently, I have been interested in more competitive " +
+                    "games such as Rainbow Six and Super Smash Bros \n" +
+                    "Aside from video games and music, I also enjoy TV Shows and manga. My " +
+                    "favorite TV Series is Game of Thrones and my favorite manga of all time is One Piece.\n",
         "Awards": [
                 "1st place - UTRGV Engineering Week Innovation Challenge",
         ],
@@ -94,21 +130,23 @@ app.get('/', (req, res) => {
 
         ]
     }
-    var params = {
+    // shows the latest tweets from user with name 'screen_name'
+    const params = {
         screen_name: 'AntonioAdame3',
-        count: 4,
+        count: 3,
         exclude_replies: true,
     }
-    var tweetInfo = {"tweets": []};
-    var promise = new Promise(function(resolve, reject){
+    let tweetInfo = {"tweets": []};
+    const promise = new Promise(function(resolve, reject){
         client.get('statuses/user_timeline', params, (error, tweets,response)=>{
             if (!error) {
                 tweets.map((t,idx)=>{
-                    var tmp = {};
-                    tmp["text"] = t.text;
-                    tmp["created_at"] = t.created_at;
-                    tmp["screen_name"] = t.user.screen_name;
-                    tmp["profile_image"] = t.user.profile_image_url;
+                    let tmp = {
+                        text: t.text,
+                        created_at: t.created_at,
+                        screen_name: t.user.screen_name,
+                        profile_image: t.user.profile_image_url
+                    };
                     tweetInfo.tweets.push(tmp);
                 });
 
@@ -123,7 +161,7 @@ app.get('/', (req, res) => {
     promise.then(function(success) {
         res.render('index', ({
             "personal_information": personal_information,
-            "tweet_info": tweetInfo
+            "tweet_info": tweetInfo,
         }));
     })
 
